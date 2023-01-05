14c2e13c721137bf5888053b7f2019028b23927c
max.sagan <max.sagan@yieldbroker.com>
2023-01-05 17:23:23 +1100

    add trpc

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,14 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 
+import { trpc } from '../utils/trpc';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  
+  const hello = trpc.hello.useQuery({ text: 'client' });
+
   return (
     <>
       <Head>
@@ -18,15 +23,23 @@ export default function Home() {
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
+            <code className={styles.code}>pages/index.tsx</code>!!
           </p>
+          {hello.data ? (
+            <p>
+              Get started by editing&nbsp;
+              <code className={styles.code}>{hello.data.greeting}</code>!!
+            </p>
+          ) : (
+            <div>Loading...</div>
+          )}
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -119,5 +132,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
